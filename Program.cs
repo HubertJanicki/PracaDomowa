8017064eaf1ec4f8d38b5ffe34815a924e3de5f3
Hubert Janicki <63345021+HubertJanicki@users.noreply.github.com>
2021-10-03 18:05:04 +0200

    PracaDomowa_T2_L4_Z2

--- Program.cs
+++ Program.cs
@@ -6,37 +6,11 @@ namespace PracaDomowa
     {
         static void Main(string[] args)
         {
-            string name, surname;
-            int age;
-            char sex;
-            long pesel;
-            long workerID;
+            char letter1 = 'a';
+            char letter2 = 'b';
+            char letter3 = 'c';
 
-            Console.WriteLine("Jak masz na imię?");
-            //string name = "Hubert";
-            name = Console.ReadLine();
-
-            Console.WriteLine("Jak masz na nazwisko?");
-            //string surname = "Janicki";
-            surname = Console.ReadLine();
-
-            Console.WriteLine("Twój wiek?");
-            //int age = 32;
-            age = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Twoja płeć? (m - mężczyzna, k - kobieta)");
-            //string sex = "m";
-            sex = char.Parse(Console.ReadLine());
-
-            Console.WriteLine("Jaki jest twój PESEL?");
-            //long pesel = 89121400000;
-            pesel = long.Parse(Console.ReadLine());
-
-            Console.WriteLine("Jaki jest twój numer pracownika?");
-            //long workerID = 3123456789;
-            workerID = long.Parse(Console.ReadLine());
-
-            Console.WriteLine($"Pracownik miesiąca to: \r\n Imię: {name} \r\n Nazwisko: {surname} \r\n Wiek: {age} \r\n Płeć: {sex} \r\n PESEL (we trust in your honesty): {pesel} \r\n Nr pracownika: {workerID} \r\n ");
+            Console.WriteLine($"Podane przez Ciebie litery w odwrotnej kolejności to: {letter3}, {letter2}, {letter1}");
         }
     }
 }
